d9accb7d1d49f838d5c82f338158592b4f29b3d8
mbsoftlab <musikbeatsSoftwarelab@outlook.de>
2020-12-06 10:57:41 +0100

    Refactor Documentation of ISwaggerUiCustomHeaderConfig

--- MbSoftLab.SwaggerUiHeaderBuilder/SwaggerUiOptionsHeadContentExtensions.cs
+++ MbSoftLab.SwaggerUiHeaderBuilder/SwaggerUiOptionsHeadContentExtensions.cs
@@ -1,11 +1,9 @@
 ﻿using Swashbuckle.AspNetCore.SwaggerUI;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace MbSoftLab.SwaggerUiHeaderBuilder
 {
-   public static class SwaggerUiOptionsHeadContentExtensions
+    public static class SwaggerUiOptionsHeadContentExtensions
     {
         public static void UseCustomHeader(this SwaggerUIOptions options, Action<SwaggerUiHeaderBuilder> header)
         {
